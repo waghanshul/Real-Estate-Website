a4b7b5a2a10342c418681d608b33eca0b9349e97
Anshul Wagh <anshulsanjaywagh@gmail.com>
2024-10-06 18:54:48 +0530

    userRoute and userCntrl setup

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import { userRoute } from './routes/userRoute.js';
 dotenv.config()
 
-nothing just fs of
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -16,3 +16,5 @@ app.use(cors())
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`);   
 });
+
+app.use('/api/user', userRoute)
