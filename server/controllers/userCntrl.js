5ae76e094ccaab33773c2c5d7daa9933f48c81ba
Anshul Wagh <anshulsanjaywagh@gmail.com>
2024-10-07 12:43:00 +0530

    message addition in usrcntrl

--- server/controllers/userCntrl.js
+++ server/controllers/userCntrl.js
@@ -7,7 +7,18 @@ export const createUser = asyncHandler(async(req, res) => {
 console.log("creating a user");
 
 let {email} = req.body;
+const userExists = await prisma.user.findUnique({where: {email: email}})
+if(!userExists) {
 
-console.log(email)
+    const user = await prisma.user.create({data: req.body})
+    res.send({
+        message: "User registered successfully",
+        user: user
+    })
+}
+
+
+
+
 
 });
