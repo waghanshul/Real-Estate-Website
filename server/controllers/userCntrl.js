3549ce7ba331a01aafab2cd0f6339c5b0f0d759d
Anshul Wagh <anshulsanjaywagh@gmail.com>
2024-10-07 12:53:22 +0530

    usercntrl update

--- server/controllers/userCntrl.js
+++ server/controllers/userCntrl.js
@@ -9,13 +9,13 @@ console.log("creating a user");
 let {email} = req.body;
 const userExists = await prisma.user.findUnique({where: {email: email}})
 if(!userExists) {
-
-    const user = await prisma.user.create({data: req.body})
+    const user = await prisma.user.create({ data: req.body })
     res.send({
         message: "User registered successfully",
         user: user
-    })
+    });
 }
+else res.status(201).send({ message: "User already registered" });
 
 
 
