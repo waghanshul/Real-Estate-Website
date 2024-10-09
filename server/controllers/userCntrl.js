58ca67d456df01590f225712b23a54aabd8fd9b3
Anshul Wagh <anshulsanjaywagh@gmail.com>
2024-10-10 00:32:13 +0530

    usrcntrl update

--- server/controllers/userCntrl.js
+++ server/controllers/userCntrl.js
@@ -17,3 +17,37 @@ if(!userExists) {
 } else res.status(201).send({ message: "User already registered" });
 
 });
+
+// function to book a visit to a resd
+
+export const bookVisit = asyncHandler(async(req, res)=>{
+    const {email, date} = req.body
+    const {id} = req.params
+
+    try{
+        
+        const alreadyBooked = await prisma.user.findUnique({
+            where: {email},
+            select: {bookedVisits: true},
+        });
+
+        if(alreadyBooked.bookedVisits.some((visit)=> visit.id === id)) {
+            res
+            .status(400)
+            .json({message: "This residency is already booked by you"});
+        }
+        else{
+            await prisma.user.update({
+                where: {email: email},
+                data: {
+                    bookedVisits: {push: {id, date}},
+                },
+            });
+        }
+        res.send("Your visit is booked successfully");
+
+    }catch(err){
+        throw new Error(err.message)
+    }
+
+});
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createUser } from "../controllers/userCntrl.js";
+import { bookVisit, createUser } from "../controllers/userCntrl.js";
 const router = express.Router();
 
 
 router.post("/register", createUser);
+router.post("/bookVisit/:id", bookVisit)
 
 export {router as userRoute}
