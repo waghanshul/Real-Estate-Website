3a81556467980ab392e8cdf43e54cc62150c41d8
Anshul Wagh <anshulsanjaywagh@gmail.com>
2024-10-09 23:40:50 +0530

    resdcntrl and residencyroute update

--- server/routes/residencyRoute.js
+++ server/routes/residencyRoute.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { createResidency, getAllResidencies } from "../controllers/resdCntrl.js";
+import { createResidency, getAllResidencies, getResidency } from "../controllers/resdCntrl.js";
 const router = express.Router();
 
 
 router.post("/create", createResidency)
 router.get("/allresd", getAllResidencies)
-
+router.get("/:id", getResidency)
 export {router as residencyRoute}
